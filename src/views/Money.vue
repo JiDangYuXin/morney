f46933adb7a2010b838d9679988000fb10197a71
cybill <1007518429@qq.com>
2020-09-07 15:24:06 +0800

    Money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,8 @@
 <template>
-<div>
-  Money.vue
+<div class="nav-wrapper">
+  <div class="content">
+    Money.vue
+  </div>
 <Nav/>
 </div>
 
@@ -16,5 +18,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper{
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content{
+  flex-grow: 1;
+  overflow: auto;
+}
 </style>
