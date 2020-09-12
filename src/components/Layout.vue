d9e40bf987cf59512a0384134939cd0c92de1898
cybill <1007518429@qq.com>
2020-09-12 10:45:18 +0800

    update

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
+  <div class="layout-wrapper" :class="classPrefix  &&  `${classPrefix}-wrapper`" >
+    <div class="content" :class="classPrefix  &&  `${classPrefix}-content`">
       <slot/>
     </div>
     <Nav/>
@@ -9,19 +9,20 @@
 
 <script lang="ts">
 export default {
-name: "Layout"
+  props: ['classPrefix'],
+  name: "Layout"
 }
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper{
-  border: 1px solid green;
+.layout-wrapper {
+
   display: flex;
   flex-direction: column;
   height: 100vh;
 }
 .content{
-  border: 1px solid blue;
+
   flex-grow: 1;
   overflow: auto;
 }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,17 +1,36 @@
 <template>
-<div>
-  <Layout>
-    <p>Money.vue</p>
+  <Layout class-prefix="layout">
+    <NumberPad/>
+    <Types/>
+    <Notes/>
+    <Tags/>
   </Layout>
-<Nav/>
-</div>
+
 
 </template>
 <script lang="ts">
-
+import NumberPad from "@/components/Money/NumberPad.vue";
+import Types from "@/components/Money/Types.vue";
+import Notes from "@/components/Money/Notes.vue";
+import Tags from "@/components/Money/Tags.vue";
 export default {
    name: "Money",
-
+  components: {Tags, Notes, Types, NumberPad},
 }
 </script>
 
+<style lang="scss">
+.layout-content{
+  display: flex;
+  flex-direction: column-reverse;
+}
+.layout-wrapper{
+
+}
+</style>
+
+
+<style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+
+</style>
