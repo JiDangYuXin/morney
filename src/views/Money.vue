80876253ce8d7917304c0b4d5d1f2c1e2b66e4d1
cybill <1007518429@qq.com>
2020-09-13 16:19:21 +0800

    update

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout class-prefix="layout">
-    {{recprd}}
-    <NumberPad @update:value="onUpdateAmount"/>
+    {{record}}
+    <NumberPad @update:value="onUpdateAmount" @submit="saveRecord"/>
     <Types :value.sync="recprd.type" />
     <Notes @update:value="onUpdateNotes"/>
     <Tags :data-source.sync="tags"  @update:value='onUpdateTags'/>
@@ -13,7 +13,7 @@ import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
-import {Component} from "vue-property-decorator";
+import {Component, Watch} from "vue-property-decorator";
 
 
 
@@ -23,6 +23,7 @@ type Record ={
   notes: string;
   type: string;
   amount: number;
+  createdAt?:Date;
 }
 
 
@@ -32,7 +33,10 @@ type Record ={
 )
 export default class Money extends Vue{
   tags=['衣','食','住','行','彩票'];
-  recprd: Record={tags:[],notes:'',type:'-',amount:0};
+  recordList: Record[]=JSON.parse(window.localStorage.getItem('recordList') ||'[]');
+  recprd: Record={
+    tags:[],notes:'',type:'-',amount:0
+  };
 
   onUpdateTags(value: string[]){
     this.recprd.tags=value;
@@ -44,6 +48,17 @@ export default class Money extends Vue{
   // onUpdateAmount(value:string){
   //   this.recprd.amount=parseFloat(value);
   // }
+
+  saveRecord(){
+    const record2:Record =JSON.parse(JSON.stringify(this.recprd)) ;
+    record2.createdAt= new Date();
+    this.recordList.push(record2);
+
+  }
+  @Watch('recordList')
+  onRecordListChange(){
+    window.localStorage.setItem('recordList',JSON.stringify(this.recordList))
+  }
 }
 </script>
 
