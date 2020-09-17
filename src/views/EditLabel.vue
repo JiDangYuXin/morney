d97ad9060ac081fc5b347ce013c72ffea7c0ed5b
cybill <1007518429@qq.com>
2020-09-17 21:18:50 +0800

    展示 tag 的 name

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -8,7 +8,7 @@
    <span class="rightIcon"></span>
  </div>
   <div class="form-wrapper">
-    <FormItem field-name="标签名" placeholder="请输入标签名"/>
+    <FormItem :value="tag.name"  field-name="标签名" placeholder="请输入标签名"/>
   </div>
 <div class="button-wrapper">
   <Button>删除标签</Button>
@@ -16,23 +16,26 @@
 </Layout>
 </template>
 
-<script>
+<script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import tagListModel from "@/models/tagListModel.ta";
-import FormItem from "@/components/Money/Formltem";
+import FormItem from "@/components/Money/Formltem.vue";
+import Button from '@/components/Button.vue';
 
 @Component({
   components: {FormItem}
 })
 export default class EditLabel extends Vue{
-created(){
+  tag?: { id: string;name: string } = undefined;
+
+created() {
   const id = this.$route.params.id
   tagListModel.fetch();
   const tags = tagListModel.data;
   const tag = tags.filter(t => t.id ===id)[0];
   if (tag){
-    console.log(tag);
+   this.tag = tag ;
   }else {
     this.$router.replace('/404');
   }
