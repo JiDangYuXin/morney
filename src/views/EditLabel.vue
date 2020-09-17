b2ce2f7127dafd92bf10eed2599fa0703ba862a6
cybill <1007518429@qq.com>
2020-09-17 22:20:06 +0800

    可以编辑和删除标签

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,17 +1,20 @@
 <template>
 <Layout>
  <div class="navBar">
-   <Icon class="leftIcon" name="left"/>
+   <Icon class="leftIcon" name="left" @click="goBack"/>
    <span class="title">
      编辑标签
    </span>
    <span class="rightIcon"></span>
  </div>
   <div class="form-wrapper">
-    <FormItem :value="tag.name"  field-name="标签名" placeholder="请输入标签名"/>
+    <FormItem :value="tag.name"
+              @update:value="update"
+              field-name="标签名"
+              placeholder="请输入标签名"/>
   </div>
 <div class="button-wrapper">
-  <Button>删除标签</Button>
+  <Button @click="remove">删除标签</Button>
 </div>
 </Layout>
 </template>
@@ -24,12 +27,12 @@ import FormItem from "@/components/Money/Formltem.vue";
 import Button from '@/components/Button.vue';
 
 @Component({
-  components: {FormItem}
+  components: {Button, FormItem}
 })
 export default class EditLabel extends Vue{
   tag?: { id: string;name: string } = undefined;
 
-created() {
+created()  {
   const id = this.$route.params.id
   tagListModel.fetch();
   const tags = tagListModel.data;
@@ -39,6 +42,19 @@ created() {
   }else {
     this.$router.replace('/404');
   }
+}
+update(name: string){
+  if(this.tag){
+    tagListModel.update(this.tag.id, name)
+  }
+}
+remove(){
+  if (this.tag){
+    tagListModel.remove(this.tag.id)
+  }
+}
+goBack(){
+  this.$router.back();
 }
 }
 </script>
